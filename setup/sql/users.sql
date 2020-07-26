36338ac373324c6da30f8bcf49c69d5d6a352f9e
Chason Deshotel <me@chasondeshotel.com>
2020-07-26 05:07:14 +0000

    added slug constraint

--- setup/sql/users.sql
+++ setup/sql/users.sql
@@ -1,14 +1,14 @@
 create table djapp.users (
 	id serial primary key
 	, username text
-	, password_salt text
-	, password_hash text
+	, password_salt text --not null
+	, password_hash text --not null
 	, name_first text
 	, name_last text
-	, name_dj text
-	, slug text unique
+	, name_dj text not null
+	, slug text not null unique
 	, bio text
-	, email text
+	, email text not null
 	, is_live boolean not null default false
 	, enabled boolean not null default false
 )
