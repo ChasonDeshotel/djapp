423536d5fb3f7b9f89281b213c43b9663402005b
Chason Deshotel <me@chasondeshotel.com>
2020-08-01 19:48:33 +0000

    refactor

--- lib/DJApp/Model/Auth.pm
+++ lib/DJApp/Model/Auth.pm
@@ -1,23 +1,11 @@
 package DJApp::Model::Auth;
 use Moo;
 use Types::Standard qw(Str Bool Undef);
-
 use DJApp::DB;
 
 my $dbh = DJApp::DB->new();
 
-## need to actually implement this
-sub _build_is_authorized {
-	my $self = shift;
-
-	if ($self->username eq 'test' and $self->password eq 'pw') {
-		return 1;
-	}
-
-	return 0;
-}
-
-sub _build_auth_key {
+sub _build_user_key {
 	my $self = shift;
 
 	if ($self->username eq 'test' and $self->password eq 'pw') {
@@ -27,13 +15,7 @@ sub _build_auth_key {
 	return undef;
 }
 
-has is_authorized => (
-	is    => 'rw'
-	, isa => Bool
-	, default => 0
-);
-
-has auth_key => (
+has user_key => (
 	is    => 'lazy'
 	, isa => Str|Undef #undef if auth fails
 );
--- lib/DJApp/Model/User.pm
+++ lib/DJApp/Model/User.pm
@@ -6,35 +6,39 @@ use MooX::Types::MooseLike::Base qw(ArrayRef);
 #use namespace::clean;
 use DJApp::Model::Mix;
 use DJApp::DB;
+use DJApp::Controller::Auth;
 
 my $dbh = DJApp::DB->new();
 
-## sub BUILD {
-# if supplied with username/password, do auth
-# return sensitive data only if auth'd
-#}
+sub BUILD {
+	my ($self, $args) = @_;
+	die "must have at least one of the following: username, user_key"
+		unless (exists $args->{username} || exists $args->{user_key});
+}
 
 sub ins {
 	my $self = shift;
 	my $sth = $dbh->prepare('insert into djapp.users (email, username) values (?,?)');
-	return $sth->execute($self->artist,$self->title);
+	$sth->execute($self->artist,$self->title) or die $sth->errstr;
+	return $sth->errstr || 1;
 }
 
-sub _build_slug {
-	my $self = shift;
-	return 0;
-	my ($slug) = $dbh->selectrow_array('select slug from djapp.users where id = ? limit 1', undef, $self->id);
-	return $slug;
+sub set_live {
+	my ($self, $is_live) = @_;
+	my $sth = $dbh->prepare('update djapp.users set is_live = ? where user_key = ?');
+	$sth->execute($is_live,$self->user_key) or die $sth->errstr;
+	return $is_live;
 }
 
 sub _build_is_live {
 	my $self = shift;
-	my ($is_live) = $dbh->selectrow_array('select is_live from djapp.users where id = ? limit 1', undef, $self->id);
+	my ($is_live) = $dbh->selectrow_array('select is_live from djapp.users where username = ? limit 1', undef, $self->username);
 	return $is_live;
 }
 
 sub _build_live_mix {
 	my $self = shift;
+
 	if ($self->is_live) {
 		my ($current_mix_id) = $dbh->selectrow_array('select * from djapp.mixes where user_id = ? order by date_live desc limit 1', undef, $self->id);
 		return DJApp::Model::Mix->new({id => $current_mix_id});
@@ -45,51 +49,43 @@ sub _build_live_mix {
 
 sub _build_email {
 	my $self = shift;
-	my ($email) = $dbh->selectrow_array('select email from djapp.users where slug = ? limit 1', undef, $self->slug);
+	my ($email) = $dbh->selectrow_array('select email from djapp.users where username = ? limit 1', undef, $self->username);
 	return $email;
 }
 
 sub _build_theme_color {
 	my $self = shift;
-	my ($theme_color) = $dbh->selectrow_array('select theme_color from djapp.users where slug = ? limit 1', undef, $self->slug);
+	my ($theme_color) = $dbh->selectrow_array('select theme_color from djapp.users where username = ? limit 1', undef, $self->username);
 	return $theme_color || 'light-blue accent-4';
 }
 
-sub _build_id {
+sub _build_username {
 	my $self = shift;
-	return '12345';
-
-	my ($id) = $dbh->selectrow_array('select id from djapp.users where slug = ? limit 1', undef, $self->slug);
-	return $id;
+	my ($username) = $dbh->selectrow_array('select username from djapp.users where user_key = ? limit 1', undef, $self->user_key);
+	return $username;
 }
-
-#sub _build_username {
-#	my $self = shift;
-#	my ($username) = $dbh->selectrow_array('select username from djapp.users where slug = ? limit 1', undef, $self->slug);
-#	return $username;
-#}
 
 sub _build_name_first {
 	my $self = shift;
-	my ($name_first) = $dbh->selectrow_array('select name_first from djapp.users where slug = ? limit 1', undef, $self->slug);
+	my ($name_first) = $dbh->selectrow_array('select name_first from djapp.users where username = ? limit 1', undef, $self->username);
 	return $name_first;
 }
 
 sub _build_name_last {
 	my $self = shift;
-	my ($name_last) = $dbh->selectrow_array('select name_last from djapp.users where slug = ? limit 1', undef, $self->slug);
+	my ($name_last) = $dbh->selectrow_array('select name_last from djapp.users where username = ? limit 1', undef, $self->username);
 	return $name_last;
 }
 
 sub _build_name_dj {
 	my $self = shift;
-	my ($name_dj) = $dbh->selectrow_array('select name_dj from djapp.users where slug = ? limit 1', undef, $self->slug);
+	my ($name_dj) = $dbh->selectrow_array('select name_dj from djapp.users where username = ? limit 1', undef, $self->username);
 	return $name_dj;
 }
 
 sub _build_bio {
 	my $self = shift;
-	my ($bio) = $dbh->selectrow_array('select bio from djapp.users where slug = ? limit 1', undef, $self->slug);
+	my ($bio) = $dbh->selectrow_array('select bio from djapp.users where username = ? limit 1', undef, $self->username);
 	return $bio;
 }
 
@@ -102,10 +98,11 @@ sub _build_mixes {
 	return \@mixes;
 }
 
-has slug => (
-	is => 'lazy'
-	, isa => Str
-);
+sub _build_id {
+	my $self = shift;
+	my ($id) = $dbh->selectrow_array('select id from djapp.users where username = ? limit 1', undef, $self->username);
+	return $id;
+}
 
 has is_live => (
 	is    => 'lazy'
@@ -126,10 +123,15 @@ has email => (
 	, isa => Str|Undef
 );
 
-#has username => (
-#	is    => 'lazy'
-#	, isa => Str
-#);
+has username => (
+	is    => 'lazy'
+	, isa => Str
+);
+
+has password => (
+	is    => 'rw'
+	, isa => Str
+);
 
 has name_first => (
 	is    => 'lazy'
@@ -161,18 +163,16 @@ has id  => (
 	, isa => Int
 );
 
-has auth_key  => (
+has user_key  => (
 	is    => 'ro'
 	, isa => Str
 );
 
-has is_authorized => (
-	is    => 'rw'
-	, isa => Bool
-	, default => 0
-);
-
-1;
+#has is_authorized => (
+#	is    => 'rw'
+#	, isa => Bool
+#	, default => 0
+#);
 
 #has schedule => (
 #	is    => 'lazy'
